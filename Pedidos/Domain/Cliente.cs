ebc03e1a85fe39505f3faed1d0b0bb2c6d76d027
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-03-08 09:10:55 -0300

    'DataAnnotations'

--- Pedidos/Domain/Cliente.cs
+++ Pedidos/Domain/Cliente.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pedidos.Domain
 {
+    [Table("Clientes")]
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Column("Phone")]
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
